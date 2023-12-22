8c0117d8c651ea3400077a4f3b7e1f7f1fc836bc
Anne Collier <apcm@cin.ufpe.br>
2023-12-22 11:53:09 -0300

    Update client.py

--- Qlearning-main/client.py
+++ Qlearning-main/client.py
@@ -13,14 +13,14 @@ class QLearning:
     def Q_values(self, next_state, reward): # equação geral para a utilidade de um estado em Q-learning (Q-values)
         return reward + self.gamma * max(self.matriz_resultado[next_state])
 
-    def best_action(self, estado):
-        return np.argmax(self.matriz_resultado[estado]) # Retorna o índice da ação com o maior valor na matriz_resultado para o estado dado.
+    def best_action(self, estado): # Retorna o índice da ação com o maior valor na matriz_resultado para o estado dado.
+        return np.argmax(self.matriz_resultado[estado])
 
     def choose_action(self, estado, epsilon):
-        if rd.random() < epsilon: 
-            return rd.choice(self.actions) # retorna uma ação aleatória
+        if rd.random() < epsilon:
+            return rd.choice(self.actions), 'aleatoria' # retorna uma ação aleatória
         else:
-            return self.actions[self.best_action(estado)] # retorna a ação com a maior utilidade estimada
+            return self.actions[self.best_action(estado)], 'melhor acao' # retorna a ação com a maior utilidade estimada
 
     def executar(self):
         conexao = cn.connect(2037)
@@ -28,14 +28,19 @@ class QLearning:
         epsilon = 0.1 # a probabilidade de escolher uma ação aleatória vai ser de 10%
 
         while True:
+            if epsilon > 0.1: #vai diminuindo o epsilon ao longo do treinamento
+                epsilon -= 0.0001
+            print(epsilon)
             print(estado_atual)
-            acao = self.choose_action(estado_atual, epsilon) # chama o método choose_action para obter uma ação com base no estado atual e na aleatoriedade
+            acao, tipo = self.choose_action(estado_atual, epsilon)  # chama o método choose_action para obter uma ação com base no estado atual e na aleatoriedade ou na melhor acao
             print(f'Ação escolhida para o estado {estado_atual}: {acao}')
 
             col_acao = self.actions.index(acao)
-            # obtém o próximo estado e a recompensa
-            estado, recompensa = cn.get_state_reward(conexao, acao)
-            plataforma, direcao = int(estado[:7], 2),int(estado[7:], 2) # converte o estado de binário para decimal.
+
+            estado, recompensa = cn.get_state_reward(conexao, acao) #obtém o próximo estado e a recompensa
+            if recompensa == -100: #com o objetivo de nao prejudicar o treinamento com os bugs do jogo
+                self.alpha = 0.1
+            plataforma, direcao = int(estado[:7], 2),int(estado[7:], 2) # converte o estado de binário para decimal e separa entre plataf e direcao
             print(plataforma, direcao)
             next_state = int(estado,2) # atualiza próximo estado
 
@@ -45,12 +50,12 @@ class QLearning:
             print(f'valor novo dessa ação: {self.matriz_resultado[estado_atual][col_acao]}')
 
             estado_atual = next_state
-
-            np.savetxt('resultado.txt', self.matriz_resultado, fmt="%f") # salva a matriz_resultado em um arquivo txt
             
+            np.savetxt('resultado.txt', self.matriz_resultado, fmt="%f") # salva a matriz_resultado em um arquivo txt
+
 # Parâmetros
-alpha = 0.7
-gamma = 0.7
+alpha = 0.1
+gamma = 0.99
 acoes = ["left", "right", "jump"]
 
 # Cria instância da classe QLearning
